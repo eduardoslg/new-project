cdbc97c1d2c271cd5932281dd1e2a262b84cd027
eduardoslg <eduardo.seve@hotmail.com>
2022-10-03 19:39:59 -0300

    att adicionando páginas

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,12 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
 import { TelaPrincipal } from '../pages/TelaPrincipal';
 import { TelaSecundaria } from '../pages/TelaSecundaria';
+import { Erro } from '../pages/Erro';
+import { Favoritos } from '../pages/Favoritos';
+import { Filmes } from '../pages/Filmes';
 
 export default function Router(): JSX.Element {
   return (
     <Routes>
       <Route path="/" element={<TelaPrincipal />} />
-      <Route path="/secundaria" element={<TelaSecundaria />} />
+      <Route path="/filme/:id" element={<Filmes />} />
+      <Route path="/favoritos" element={<TelaPrincipal />} />
+
+      <Route path="*" element={<TelaSecundaria />} />
     </Routes>
   );
 }
